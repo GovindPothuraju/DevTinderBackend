ed5abbd19b3b62816063aa756b1c4cb3482d3262
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-12-16 19:10:17 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=express();
 
 app.use('/test',(req,res)=>{
-    res.send('hello from govind');
+    res.send('hello from govidha');
 })
 
 const port=7777;
