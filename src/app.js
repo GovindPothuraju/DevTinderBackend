0de5ba9d077c2c4fa9c967d61a604692528d039c
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-12-16 19:06:32 +0530

    app is created

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app=express();
+
+app.use('/test',(req,res)=>{
+    res.send('Hello World!');
+})
+
+const port=7777;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
