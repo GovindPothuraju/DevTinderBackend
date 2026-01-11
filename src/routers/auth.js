609add1749ca6cfaf581470203888a5942bb0631
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-11 23:10:07 +0530

    Sign up errors solved

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -30,7 +30,16 @@ authRouter.post("/signup",async (req, res)=>{
     //createuser and save to db
     const newUser=new User({firstName,lastName,email,password:passwordHashed});
 
-    await newUser.save();
+    const savedUser= await newUser.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token",token,{
+          httpOnly: true,
+          secure: true,          // REQUIRED for Render (HTTPS)
+          sameSite: "None", 
+          maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
     res.status(201).json({
       success:true,
       message:"User signed up successfully",
